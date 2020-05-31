917f51aeb9fd9c17ea453ba3bbdef38a08542a64
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2020-05-31 14:31:34 +0200

    animation in js instead of css

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const mainContainer = document.getElementById("main-container");
 const btnPrev = document.getElementById("btn-prev");
 const btnNext = document.getElementById("btn-next");
 let slideNum, slides, dots;
+let interval = 6;
 
 function initGallery() {
     slideNum = 0;
@@ -31,41 +32,93 @@ function moveByOne(n) {
 btnPrev.addEventListener("click", () => {
     moveByOne(-1);
 });
+
 btnNext.addEventListener("click", () => {
     moveByOne(1);
 });
 
-function moveSlide(n) {
-    let i, current, next;
-    const moveSlideAnimClass = {
-        forCurrent: "",
-        forNext: ""
+function moveLeftCurrent(elem) {  
+    let pos = 0;
+    let id = setInterval(frame, interval);
+    function frame() {
+      if (pos == -100) {
+        clearInterval(id);
+      } else {
+        pos--; 
+        elem.style.left = pos + '%'; 
+      }
     }
+  }
+
+function moveLeftNext(elem) { 
+    let pos = 100;
+    let id = setInterval(frame, interval);
+    function frame() {
+        if (pos == 0) {
+        clearInterval(id);
+        } else {
+        pos--; 
+        elem.style.left = pos + '%'; 
+        elem.style.opacity = 1;
+        }
+    }
+}
+
+function moveRightCurrent(elem) {  
+    let pos = 0;
+    let id = setInterval(frame, interval);
+    function frame() {
+      if (pos == 100) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.left = pos + '%'; 
+      }
+    }
+  }
+
+function moveRightNext(elem) { 
+    let pos = -100;
+    let id = setInterval(frame, interval);
+    function frame() {
+        if (pos == 0) {
+        clearInterval(id);
+        } else {
+        pos++; 
+        elem.style.left = pos + '%'; 
+        elem.style.opacity = 1;
+        }
+    }
+}
+
+function moveSlide(n) {
+    let i;
+    let next=slides[n];
+    let current=slides[slideNum];
 
     if (n>slideNum) {
         if (n>=slides.length) {
             n = 0;
+            next=slides[0];
         }
-        moveSlideAnimClass.forCurrent = "move-left-current-slide";
-        moveSlideAnimClass.forNext = "move-left-next-slide";
+
+        moveLeftNext(next);
+        moveLeftCurrent(current);
     }
     else if (n<slideNum) {
         if (n<0) {
             n = slides.length - 1;
+            next = slides[slides.length-1]
         }
-        moveSlideAnimClass.forCurrent = "move-right-current-slide";
-        moveSlideAnimClass.forNext = "move-right-next-slide";
+
+        moveRightNext(next);
+        moveRightCurrent(current);
     }
     if (n !== slideNum) {
-        next=slides[n];
-        current=slides[slideNum];
         for (i=0; i<slides.length; i++) {
             slides[i].className = "slide-img";
-            slides[i].style.opacity = 0;
             dots[i].style.backgroundColor = "#b0b0b0";
         }
-        current.classList.add(moveSlideAnimClass.forCurrent);
-        next.classList.add(moveSlideAnimClass.forNext);
         dots[n].style.backgroundColor = "#7a7a7a";
         slideNum = n;
     }
@@ -74,12 +127,12 @@ function moveSlide(n) {
 let timer = null;
 
 function start() {
-    timer = setInterval(() => moveByOne(1), 2000)
+    timer = setInterval(() => moveByOne(1), 2500)
 };
 
 function pause() {
     clearInterval(timer)
-}
+};
 
 mainContainer.addEventListener("mouseenter", pause);
 mainContainer.addEventListener("mouseleave", start);
